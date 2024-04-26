7a8d85c7b324b266f713744c0fa24c0a7b06fff3
Low <54532616+Lowkh@users.noreply.github.com>
2024-04-26 23:16:42 +0800

    Update Practice3Test.java

--- Practice3Test.java
+++ Practice3Test.java
@@ -10,7 +10,7 @@ public class UserTest{
     @Test
     public void testUser(){
         user = new User("John Doe", "MAD Developer", 1, false);
-        assertEquals("JohnDoe", user.name);
+        assertEquals("John Doe", user.name);
         assertEquals("MAD Developer", user.description);
         assertEquals(1, user.id);
         assertEquals(false, user.followed);
